eb4e2f9dc16001d39b104c3e105480caf065bbb9
Emil Mladenov <emladenoff@gmail.com>
2017-02-03 22:35:27 +0000

    added initial files, entry and leave animations + gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+gulp.task('sass',function() {
+  return gulp.src('./src/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./src'));
+});
+
+gulp.task('autoprefixer',function() {
+  return gulp.src('./src/*.css')
+  .pipe(autoprefixer({
+    browsers : ['last 4 versions'],
+    cascade : false
+  }))
+  .pipe(gulp.dest('./src'));
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('./src/*.css')
+        .pipe(cleanCSS({debug: true}, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+        }))
+        .pipe(gulp.dest('./..'));
+});
+gulp.task('watch',function() {
+  gulp.watch('./src/*.scss',['sass']);
+  gulp.watch('./src/*.css',['autoprefixer','minify-css']);
+});
